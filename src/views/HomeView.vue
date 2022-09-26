23af6fcdaf8ab408a25e3271bc340787436d92bd
Mirac <miracerdin@gmail.com>
2022-09-26 17:00:18 +0300

    home cleaned

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,6 @@
     <div v-if="!allDataList">Loading..</div>
     <div class="col">
       <input type="text" v-model="search" placeholder="Search" />
-      <!-- <button v-on:click="filteredList">Search</button> -->
     </div>
     <div class="cover">
       <div v-for="data in filteredList" :key="data.name">
@@ -18,9 +17,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
-// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import { mapGetters, mapActions } from "vuex";
+import { Component, Vue } from "vue-property-decorator";
 import OnePokemon from "./OnePokemon.vue";
 import { Action, Getter } from "vuex-class";
 
@@ -28,41 +25,9 @@ import { Action, Getter } from "vuex-class";
   components: {
     OnePokemon,
   },
-  // props: {
-  //   search: {
-  //     type: String,
-  //     default: "",
-  //   },
-  // },
-
-  // computed: {
-  //   ...mapGetters(["allDataList"]),
-  //   filteredList() {
-  //     if (!this.search) {
-  //       return this.allDataList;
-  //     } else {
-  //       return this.allDataList.filter((post) => {
-  //         return post.name.toLowerCase().includes(this.search.toLowerCase());
-  //       });
-  //     }
-  //   },
-  // },
-  // methods: {
-  //   ...mapActions(["fetchPokemon"]),
-  // },
-  // created() {
-  //   this.fetchPokemon();
-  // },
 })
 export default class HomeView extends Vue {
   search = "";
-  dataList = [];
-
-  // data() {
-  //   return {
-  //     dataList: [],
-  //   };
-  // }
 
   @Action("fetchPokemon")
   fetchPokemon!: () => any;
@@ -71,29 +36,12 @@ export default class HomeView extends Vue {
 
   async mounted() {
     this.fetchPokemon();
-    // console.log("all", this.allDataList);
-
     let local = JSON.parse(localStorage.getItem("liste") as string) as [];
     if (!local || local.length === 0) {
       localStorage.setItem("liste", JSON.stringify([]));
     }
   }
-  // deneme() {
-  //   this.allDataList.filter((item) =>
-  //     item.name.toLowerCase().includes(this.search)
-  //   );
-  // }
-  // @Watch("search")
-  // filteredList() {
-  //   if (!this.search) {
-  //     return (this.dataList = this.allDataList);
-  //   } else {
-  //     return this.allDataList.filter((post): object => {
-  //       console.log(this.search);
-  //       return post.name.includes(this.search.toLowerCase());
-  //     });
-  //   }
-  // }
+
   get filteredList() {
     if (!this.search) {
       return this.allDataList;
@@ -104,10 +52,6 @@ export default class HomeView extends Vue {
       });
     }
   }
-
-  // set filteredList(value) {
-  //   this.allDataList;
-  // }
 }
 </script>
 <style scoped>

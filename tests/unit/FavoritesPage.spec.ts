25b854e9a65cb2900c4151013664aa43e15fc0bb
Mirac <miracerdin@gmail.com>
2022-09-28 08:56:58 +0300

    first localstorage test succesfull

--- tests/unit/FavoritesPage.spec.ts
+++ tests/unit/FavoritesPage.spec.ts
@@ -6,4 +6,45 @@ describe("FavoritesPage", () => {
     const wrapper = mount(FavoritesPage);
     expect(wrapper.find(".cover").exists()).toBeTruthy();
   });
+
+  const localStorageMock = (function () {
+    interface Local {
+      [key: string]: string; // key ve value ya type vermem gerekiyordu and i used an interface [key:string] and value :string. if value was a number type [key:string]:number, that would be.
+    }
+    let store: Local = {};
+
+    return {
+      getItem(key: string) {
+        return store[key];
+      },
+
+      setItem(key: string, value: string) {
+        store[key] = value;
+      },
+
+      clear() {
+        store = {};
+      },
+
+      removeItem(key: string) {
+        delete store[key];
+      },
+
+      getAll() {
+        return store;
+      },
+    };
+  })();
+
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
+  const setLocalStorage = (id: string, data: any) => {
+    window.localStorage.setItem(id, JSON.stringify(data));
+  };
+  it("data is added into local storage", () => {
+    const mockId = "1";
+    const mockJson = { data: "json data" };
+    setLocalStorage(mockId, mockJson);
+    expect(localStorage.getItem(mockId)).toEqual(JSON.stringify(mockJson));
+  });
 });
